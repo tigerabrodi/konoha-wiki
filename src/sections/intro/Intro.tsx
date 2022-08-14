5530c12abcc814e61f235d3cc2e2caef78ef9dbf
tigerabrodi <tigerabrodi@gmail.com>
2022-08-14 12:06:39 +0200

    feat: add intro section for mobile

---
+++ src/sections/intro/Intro.tsx
@@ -0,0 +1,17 @@
+import './Intro.css'
+import { NarutoMainArt } from '../../arts/NarutoMainArt'
+import { NarutoSasukeArt } from '../../arts/NarutoSasukeArt'
+
+export function Intro() {
+  return (
+    <section class="intro__section">
+      <h1 class="sr-only">Naruto</h1>
+      <NarutoMainArt class="intro__main-art" />
+      <h2 class="intro__subtitle text-with-gradient-core">
+        The tale of Naruto Uzumaki, from an orphan to the greatest ninja in
+        history.
+      </h2>
+      <NarutoSasukeArt class="intro__naruto-sasuke-art" />
+    </section>
+  )
+}
