d5e41a7d5bfc3eff57c427dea07f8cb0a2f82ef8
tigerabrodi <tigerabrodi@gmail.com>
2022-08-24 06:59:15 +0200

    feat: add background video to intro section

--- src/sections/intro/Intro.tsx
+++ src/sections/intro/Intro.tsx
@@ -1,11 +1,21 @@
 import './Intro.css'
 import { NarutoMainArt } from '../../arts/NarutoMainArt'
 import { NarutoSasukeArt } from '../../arts/NarutoSasukeArt'
+import NarutoSasukeFightVideo from '../../assets/naruto-sasuke-fight.mp4'
 
 export function Intro() {
   return (
-    <section class="section">
+    <section class="section intro__section">
       <h1 class="sr-only">Intro</h1>
+      <video
+        src={NarutoSasukeFightVideo}
+        autoplay
+        loop
+        muted
+        aria-hidden="true"
+        class="intro__video"
+      />
+      <div aria-hidden="true" class="intro__overlay" />
       <NarutoMainArt class="intro__main-art" />
       <h2 class="intro__subtitle text-with-gradient gradient-to-right">
         The tale of Naruto Uzumaki, from an orphan to the greatest ninja in
