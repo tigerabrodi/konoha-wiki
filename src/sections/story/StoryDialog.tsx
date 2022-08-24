4d909f55c0544bac3252f315028123b13a9ffc66
tigerabrodi <tigerabrodi@gmail.com>
2022-08-24 09:00:03 +0200

    feat: implement progress when reading story

--- src/sections/story/StoryDialog.tsx
+++ src/sections/story/StoryDialog.tsx
@@ -1,4 +1,4 @@
-import { Accessor, For } from 'solid-js'
+import { Accessor, createSignal, For } from 'solid-js'
 import { NarutoFullStoryArt } from '../../arts/NarutoFullStoryArt'
 import { CloseIcon } from '../../icons/CloseIcon'
 import { storyChapters } from '../../lib/constants'
@@ -10,17 +10,43 @@ export function StoryDialog(props: {
   onClose: () => void
   isOpen: Accessor<boolean>
 }) {
+  const [scrollProgress, setScrollProgress] = createSignal(0)
   let containerElement: HTMLDivElement | undefined
 
   useClickOutside(() => containerElement, props.onClose)
 
   createFocusTrap(props.isOpen, () => containerElement)
 
+  function handleScrollProgress() {
+    if (containerElement) {
+      const winScroll = containerElement.scrollTop
+      const height =
+        containerElement.scrollHeight - document.documentElement.clientHeight
+      const newScrollProgress = (winScroll / height) * 100
+
+      // Check here is necessary since you may scroll too fast and then the number would end up being 103 instead of 100.
+      setScrollProgress(newScrollProgress > 100 ? 100 : newScrollProgress)
+    }
+  }
+
   return (
     <>
       <div class="story__dialog-overlay" />
-      <div role="dialog" class="story__dialog" ref={containerElement}>
+      <div
+        role="dialog"
+        class="story__dialog"
+        ref={containerElement}
+        onScroll={handleScrollProgress}
+      >
         <h1 class="sr-only">Naruto full story</h1>
+        <div
+          class="story__progress gradient-to-right"
+          role="progressbar"
+          aria-valuemax="100"
+          aria-valuemin="0"
+          aria-valuenow={scrollProgress()}
+          style={{ transform: `scaleX(${scrollProgress() * 0.01})` }}
+        />
         <button
           type="button"
           onClick={props.onClose}
